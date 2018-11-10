a81371f029ae81f1459324affe32d6c4cdfc4840
LAKuEN <y.s.k.brave@gmail.com>
2018-11-10 19:11:07 +0900

    :feat POSTリクエストで受け取った画像から付箋を検出する処理と、切り抜いた画像をzipに固めてレスポンスとして返すように実装

--- main.go
+++ main.go
@@ -1,47 +1,106 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/LAKuEN/detect-stickynotes"
+	"github.com/LAKuEN/zip-files"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"gocv.io/x/gocv"
 )
 
-func main() {
-	filePath := flag.String("filepath", "", "target file name")
-	flag.Parse()
+// ImgFile はファイル名とgocv.Matの画像データを包含する構造体です。
+type ImgFile struct {
+	name string
+	mat  gocv.Mat
+}
 
-	substrFilePath := strings.Split(*filePath, ".")
-	dstDirPath := strings.Join(substrFilePath[0:len(substrFilePath)-1], ".")
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", getHandler)
+	e.POST("/", postHandler)
+
+	port := ":" + os.Getenv("PORT")
+	e.Start(port)
+}
+
+func getHandler(c echo.Context) error {
+	message := `# How to call the API
+$ curl -X POST {URL} -F "file=@{image file path}"`
+	return c.String(http.StatusOK, message+"\n")
+}
+
+func postHandler(c echo.Context) error {
+	imgFile, err := context2mat(c)
+	defer imgFile.mat.Close()
+	if imgFile.mat.Empty() {
+		return c.String(http.StatusBadRequest,
+			"It is not possible to process a file other than the image file\n")
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	dstDirPath := filepath.Join(cwd, imgFile.name)
 	if _, err := os.Stat(dstDirPath); os.IsNotExist(err) {
 		os.Mkdir(dstDirPath, 0777)
 	}
-	orig := gocv.IMRead(*filePath, gocv.IMReadColor)
-	if orig.Empty() {
-		onError(fmt.Sprintf("cannot read file as image: %s", *filePath))
+
+	stickies, err := stickynote.CutNDraw(imgFile.mat)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	stickies, err := stickynote.CutNDraw(orig)
-	if err != nil {
-		onError(err.Error())
-	}
 	gocv.IMWrite(filepath.Join(dstDirPath, "0_drawed.jpeg"), stickies.DrawedImg)
 	for idx, cropped := range stickies.CroppedImgs {
 		gocv.IMWrite(filepath.Join(dstDirPath, fmt.Sprintf("%v.jpeg", idx+1)),
 			cropped)
 	}
 
-	fmt.Printf("saved as %v\n", dstDirPath)
+	zipFilePath, err := zipfiles.InDir(dstDirPath)
+	if err != nil {
+		return c.String(http.StatusInternalServerError,
+			"Failed to create zip file\n")
+	}
+	zipFileBytes, err := ioutil.ReadFile(zipFilePath)
+	if err != nil {
+		return c.String(http.StatusInternalServerError,
+			"Failed to convert zip file to bytearray\n")
+	}
+
+	return c.Blob(http.StatusOK, "application/zip", zipFileBytes)
 }
 
-// FIXME 共通関数モジュールに切り出した方がいいかも？
-//       画像処理系の処理とか、処理の区分が見えているなら、区分毎にモジュール分割してもいいかも
-// onError はエラー発生時の共通処理です。
-func onError(errMsg string) {
-	fmt.Fprintln(os.Stderr, errMsg)
-	os.Exit(1)
+// context2mat はecho.Contextから画像ファイル名と画像データを
+// ImgFile構造体として取り出します。
+func context2mat(c echo.Context) (ImgFile, error) {
+	fileHeader, err := c.FormFile("file")
+	if err != nil {
+		return ImgFile{}, err
+	}
+	file, err := fileHeader.Open()
+	if err != nil {
+		return ImgFile{}, err
+	}
+	defer file.Close()
+	imgBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return ImgFile{}, err
+	}
+	img, err := gocv.IMDecode(imgBytes, gocv.IMReadColor)
+	if err != nil {
+		return ImgFile{}, err
+	}
+
+	return ImgFile{name: fileHeader.Filename, mat: img}, nil
 }
